0c206f362372213e7c42fb74f8ce514b8ce66c1f
Rafael Liexors <Rafael.Liexors.Programador@gmail.com>
2025-01-16 16:19:57 -0300

    Adicionando comentários em cada código

--- ex041.py
+++ ex041.py
@@ -1,8 +1,13 @@
-from datetime import date
+from datetime import date #Importar biblioteca de manipulação de datas
+#Criando a variável pro tempo atual
 atual = date.today().year
+#Solicitando que o usuário digite o ano de nascimento
 nascimento = int(input('Ano do nascimento: '))
+#Variável pra calcular a idade
 idade = atual - nascimento
+#resultado mostrado na tela
 print('O atleta tem {} anos.'.format(idade))
+#Adicionado a estrutura de condição para classificar os nadadores de acordo com a idade
 if idade <= 9:
     print('Classificação: MIRIM.')
 elif idade <= 14:
